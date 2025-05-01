d595874ded2dc4faf1076add9ffb192bcbd6667c
OLuizFernando <luizfernandodematoscarvalho@gmail.com>
2025-05-01 00:55:32 -0300

    change `Navbar` colors

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -12,7 +12,7 @@ function Navbar() {
 
   return (
     <>
-      <nav className="bg-neutral-800 bg-opacity-5 fixed w-full z-20 top-0 start-0">
+      <nav className="bg-[#2e2e2e] bg-opacity-5 fixed w-full z-20 top-0 start-0">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-5">
           <a href="/" className="flex items-center">
             <span className="self-center text-2xl md:text-3xl whitespace-nowrap text-white konsens font-semibold">
@@ -49,14 +49,14 @@ function Navbar() {
             className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
             id="navbar-sticky"
           >
-            <ul className="flex flex-col text-lg p-4 md:p-0 mt-4 font-medium border border-neutral-700 rounded-lg bg-neutral-800 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent">
+            <ul className="flex flex-col text-lg p-4 md:p-0 mt-4 font-medium border border-neutral-700 rounded-lg bg-[#2e2e2e] md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent">
               {navItems.map((item) => (
                 <li key={item.id}>
                   <a
                     href={item.href}
                     className={`
                       block py-2 px-3 rounded-md md:py-2 md:px-4 text-white
-                      ${pathname === item.href ? "bg-neutral-900" : "hover:bg-neutral-900"}
+                      ${pathname === item.href ? "bg-neutral-800" : "hover:bg-neutral-800"}
                     `}
                     aria-current="page"
                   >
