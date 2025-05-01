d91a5d6dcfa4c2be65aa373a92d9a188117e638b
OLuizFernando <luizfernandodematoscarvalho@gmail.com>
2025-05-01 03:21:20 -0300

    add gradient to backgroung

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
+import FeaturesSection from "@/components/FeaturesSection";
 
 function Home() {
   return (
     <>
-      <div className="h-screen flex flex-col justify-center items-center">
+      <div className="h-screen flex flex-col justify-center items-center bg-gradient-to-br from-white to-gray-300">
         <Navbar />
         <HeroSection />
       </div>
+      <FeaturesSection />
     </>
   );
 }
