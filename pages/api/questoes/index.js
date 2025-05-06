97df896746f8b515952d470bbf44bd459c3093b0
oluizfernando <luizfernandodematoscarvalho@gmail.com>
2025-05-05 21:34:06 -0300

    change `routes` filter to `url params` filter

--- pages/api/questoes/index.js
+++ pages/api/questoes/index.js
@@ -9,6 +9,28 @@ router.get(getHandler);
 export default router.handler(controller.errorHandlers);
 
 async function getHandler(request, response) {
-  const provasFound = await database.find({});
+  const disciplinaMap = {
+    biologia: "Biologia",
+    fisica: "Física",
+    geografia: "Geografia",
+    historia: "História",
+    ingles: "Inglês",
+    matematica: "Matemática",
+    multidisciplinar: "Multidisciplinar",
+    portugues: "Português",
+    quimica: "Química",
+    "raciocinio-logico": "Raciocínio Lógico",
+  };
+
+  const { ano, semestre, numero, disciplina } = request.query;
+
+  const filtro = {};
+
+  if (ano) filtro.ano = parseInt(ano, 10);
+  if (semestre) filtro.semestre = parseInt(semestre, 10);
+  if (numero) filtro.numero = parseInt(numero, 10);
+  if (disciplina) filtro.disciplina = disciplinaMap[disciplina];
+
+  const provasFound = await database.find(filtro);
   response.status(200).json(provasFound);
 }
