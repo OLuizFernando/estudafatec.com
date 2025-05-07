532102e0eba3e5f027c32b20581d28c578cc17b6
OLuizFernando <luizfernandodematoscarvalho@gmail.com>
2025-05-07 01:34:13 -0300

    refactor `PlansSection` and change some widths

--- components/PlansSection/index.js
+++ components/PlansSection/index.js
@@ -1,87 +1,90 @@
 function PlansSection() {
-  return (
-    <>
-      <div
-        id="planos"
-        className="flex justify-center bg-gradient-to-br from-white to-neutral-300 w-full text-[#2e2e2e]"
-      >
-        <div className="flex flex-col items-center max-w-screen-xl md:mx-20 px-5 py-15 md:py-25 md:pt-auto">
-          <h2 className="text-4xl md:text-5xl font-bold text-center mb-4">
-            Planos
-          </h2>
-          <p className="text-2xl md:text-3xl text-center mb-15 md:mb-20 text-neutral-600">
-            Seu caminho até a aprovação começa aqui!
-          </p>
-          <div className="flex flex-col md:flex-row text-center w-full">
-            {/* BASE */}
-            <div className="flex flex-col h-full justify-center items-center md:px-7 mb-10 md:mb-auto mx-3 flex-1 rounded-2xl shadow-lg bg-white p-9">
-              <h3 className="text-3xl md:text-4xl font-bold">Base</h3>
-              <h4 className="text-2xl md:text-3xl font-semibold my-10">
-                R$
-                <span className="text-6xl md:text-7xl font-bold mx-0.5">0</span>
-                /mês
-              </h4>
-              <div className="flex flex-col">
-                <ul className="text-xl md:text-2xl text-neutral-600 text-start mb-7">
-                  <li className="flex items-center mb-3">
-                    <img className="size-7 me-3" src="/icons/check.png" />
-                    10 Questões por dia
-                  </li>
-                  <li className="flex items-center mb-3">
-                    <img className="size-7 me-3" src="/icons/x.png" />
-                    Estatísticas do aluno
-                  </li>
-                  <li className="flex items-center">
-                    <img className="size-7 me-3" src="/icons/x.png" />
-                    Plano de estudo
-                  </li>
-                </ul>
-                <a
-                  href="#"
-                  className="text-[#2e2e2e] bg-white hover:bg-neutral-200 border-2 border-neutral-200 focus:ring-4 focus:outline-none focus:ring-neutral-100 font-medium text-xl md:text-2xl rounded-lg py-3 text-center"
-                >
-                  Experimente
-                </a>
-              </div>
-            </div>
+  const plans = [
+    {
+      id: 0,
+      name: "Base",
+      price: "0",
+      priceColor: "text-[#2e2e2e]",
+      button: {
+        text: "Experimente",
+        style:
+          "text-[#2e2e2e] bg-white hover:bg-neutral-200 border-2 border-neutral-200 focus:ring-neutral-100",
+        href: "/register",
+      },
+      features: [
+        { id: 0, text: "10 Questões por dia", icon: "/icons/check.png" },
+        { id: 1, text: "Estatísticas do aluno", icon: "/icons/x.png" },
+        { id: 2, text: "Plano de estudo", icon: "/icons/x.png" },
+      ],
+    },
+    {
+      id: 1,
+      name: "Aprovação",
+      price: "20",
+      priceColor: "text-[#922020]",
+      button: {
+        text: "Seja aprovado",
+        style:
+          "text-white bg-[#922020] hover:bg-red-900 shadow-lg focus:ring-red-300",
+        href: "/payment",
+      },
+      features: [
+        { id: 0, text: "Questões ilimitadas", icon: "/icons/check.png" },
+        { id: 1, text: "Estatísticas do aluno", icon: "/icons/check.png" },
+        { id: 2, text: "Plano de estudo", icon: "/icons/check.png" },
+      ],
+    },
+  ];
 
-            {/* APROVAÇÃO */}
-            <div className="flex flex-col h-full justify-center items-center md:px-7 mb-10 md:mb-auto mx-3 flex-1 rounded-2xl shadow-lg bg-white p-9">
-              <h3 className="text-3xl md:text-4xl font-bold">Aprovação</h3>
-              <h4 className="text-2xl md:text-3xl font-semibold my-10 text-[#922020]">
+  return (
+    <div
+      id="planos"
+      className="flex justify-center bg-gradient-to-br from-white to-neutral-300 w-full text-[#2e2e2e]"
+    >
+      <div className="flex flex-col items-center max-w-screen-xl md:mx-20 px-5 py-15 md:py-25 md:pt-auto md:w-full">
+        <h2 className="text-4xl md:text-5xl font-bold text-center mb-4">
+          Planos
+        </h2>
+        <p className="text-2xl md:text-3xl text-center mb-15 md:mb-20 text-neutral-600">
+          Seu caminho até a aprovação começa aqui!
+        </p>
+        <div className="flex flex-col md:flex-row text-center w-full md:w-2xl lg:w-3xl">
+          {plans.map((plan) => (
+            <div
+              key={plan.id}
+              className="flex flex-col h-full justify-center items-center md:px-7 mb-10 md:mb-auto mx-3 flex-1 rounded-2xl shadow-lg bg-white p-9"
+            >
+              <h3 className="text-3xl md:text-4xl font-bold">{plan.name}</h3>
+              <h4
+                className={`text-2xl md:text-3xl font-semibold my-10 ${plan.priceColor}`}
+              >
                 R$
                 <span className="text-6xl md:text-7xl font-bold mx-0.5">
-                  20
+                  {plan.price}
                 </span>
                 /mês
               </h4>
               <div className="flex flex-col">
                 <ul className="text-xl md:text-2xl text-neutral-600 text-start mb-7">
-                  <li className="flex items-center mb-3">
-                    <img className="size-7 me-3" src="/icons/check.png" />
-                    Questões ilimitadas
-                  </li>
-                  <li className="flex items-center mb-3">
-                    <img className="size-7 me-3" src="/icons/check.png" />
-                    Estatísticas do aluno
-                  </li>
-                  <li className="flex items-center">
-                    <img className="size-7 me-3" src="/icons/check.png" />
-                    Plano de estudo
-                  </li>
+                  {plan.features.map((feature) => (
+                    <li key={feature.id} className="flex items-center mb-3">
+                      <img className="size-7 me-3" src={feature.icon} />
+                      {feature.text}
+                    </li>
+                  ))}
                 </ul>
                 <a
-                  href="#"
-                  className="text-white bg-[#922020] hover:bg-red-900 shadow-lg focus:ring-4 focus:outline-none focus:ring-red-300 font-medium text-xl md:text-2xl rounded-lg py-3 text-center"
+                  href={plan.button.href}
+                  className={`${plan.button.style} focus:ring-4 focus:outline-none font-medium text-xl md:text-2xl rounded-lg py-3 text-center`}
                 >
-                  Seja aprovado
+                  {plan.button.text}
                 </a>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
