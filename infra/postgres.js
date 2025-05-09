c77f76ecc649feee72a1deba120cd271479bb85f
OLuizFernando <luizfernandodematoscarvalho@gmail.com>
2025-05-08 23:01:06 -0300

    add `infra/postgres.js` and use `Pool` instead of `Client`

---
+++ infra/postgres.js
@@ -0,0 +1,38 @@
+import { Pool } from "pg";
+import { ServiceError } from "./errors.js";
+
+const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
+});
+
+async function query(queryObject) {
+  try {
+    const result = await pool.query(queryObject);
+    return result;
+  } catch (error) {
+    const serviceErrorObject = new ServiceError({
+      message: "Erro na conexão com o Postgres ou na Query.",
+      cause: error,
+    });
+    throw serviceErrorObject;
+  }
+}
+
+async function getNewClient() {
+  return pool;
+}
+
+const database = {
+  query,
+  getNewClient,
+};
+
+export default database;
