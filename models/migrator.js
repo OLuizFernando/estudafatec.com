85fc83903a768ec5a5cb7c0d380d8d6ca5703836
Luiz Fernando <130947126+OLuizFernando@users.noreply.github.com>
2025-05-09 19:05:23 +0000

    add `migrator` model and create a test migration

---
+++ models/migrator.js
@@ -0,0 +1,52 @@
+import migrationRunner from "node-pg-migrate";
+import { resolve } from "node:path";
+import postgres from "infra/postgres";
+
+const defaultMigrationOptions = {
+  dryRun: true,
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function listPendingMigrations() {
+  let dbClient;
+
+  try {
+    dbClient = await postgres.getDb();
+
+    const pendingMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient: dbClient,
+    });
+    return pendingMigrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+async function runPendingMigrations() {
+  let dbClient;
+
+  try {
+    dbClient = await postgres.getDb();
+
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient: dbClient,
+      dryRun: false,
+    });
+
+    return migratedMigrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+const migrator = {
+  listPendingMigrations,
+  runPendingMigrations,
+};
+
+export default migrator;
