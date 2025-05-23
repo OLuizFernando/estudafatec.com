bf53c07b2db6e8c43c820cd6ffb8d6edc2beb3f3
OLuizFernando <luizfernandodematoscarvalho@gmail.com>
2025-05-22 22:47:47 -0300

    feat: create `waitlist` page

---
+++ pages/waitlist/index.js
@@ -0,0 +1,38 @@
+import CredentialsForm from "components/CredentialsForm";
+
+function Waitlist() {
+  return (
+    <>
+      <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-br from-white to-neutral-300">
+        <CredentialsForm
+          title="Lista de Espera"
+          inputs={[
+            {
+              label: "Nome",
+              id: "name",
+              type: "text",
+              placeholder: "Fulano de Tal",
+              required: true,
+              autoFocus: true,
+            },
+            {
+              label: "E-mail",
+              id: "email",
+              type: "email",
+              placeholder: "exemplo@email.com",
+              required: true,
+            },
+          ]}
+          buttonText="Registrar"
+          redirectText={{
+            label: "Caiu aqui de paraquedas?",
+            linkText: "Conheça a plataforma!",
+            href: "/",
+          }}
+        />
+      </div>
+    </>
+  );
+}
+
+export default Waitlist;
