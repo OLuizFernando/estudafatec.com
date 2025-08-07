0511721d4fad8fce813366adf1ecb084bd83945e
OLuizFernando <luizfernandodematoscarvalho@gmail.com>
2025-08-06 21:57:31 -0300

    feat: implement `/api/user` endpoint with session renewal

--- infra/postgres.js
+++ infra/postgres.js
@@ -1,16 +1,5 @@
-import { Pool } from "pg";
+import { Client } from "pg";
 import { ServiceError } from "./errors.js";
-
-const pool = new Pool({
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
-  user: process.env.POSTGRES_USER,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
-  ssl: process.env.NODE_ENV === "production" ? true : false,
-  max: 20,
-  allowExitOnIdle: true,
-});
 
 async function query(queryObject) {
   let client;
@@ -20,17 +9,27 @@ async function query(queryObject) {
     return result;
   } catch (error) {
     const serviceErrorObject = new ServiceError({
-      message: "Erro na conexão com o Postgres ou na Query.",
+      message: "Erro na conexão com Banco ou na Query.",
       cause: error,
     });
     throw serviceErrorObject;
   } finally {
-    client?.release();
+    await client?.end();
   }
 }
 
 async function getDb() {
-  return pool.connect();
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "production" ? true : false,
+  });
+
+  await client.connect();
+  return client;
 }
 
 const postgres = {
