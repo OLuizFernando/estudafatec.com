be170bb6d7628be3a52dc962da2061844a08b0e6
OLuizFernando <luizfernandodematoscarvalho@gmail.com>
2025-10-02 21:55:20 -0300

    test: add `mailcatcher` tests

--- tests/integration/api/infra/email.test.js
+++ tests/integration/api/infra/email.test.js
@@ -1,4 +1,10 @@
 import email from "infra/email";
+import orchestrator from "tests/orchestrator";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.deleteAllEmails();
+});
 
 describe("email", () => {
   test("send()", async () => {
@@ -8,5 +14,18 @@ describe("email", () => {
       subject: "Subject Test",
       text: "Body test.",
     });
+    await email.send({
+      from: "EstudaFatec <from@estudafatec.com>",
+      to: "to@estudafatec.com",
+      subject: "Last Email",
+      text: "Last email sent.",
+    });
+
+    const lastEmail = await orchestrator.getLastEmail();
+
+    expect(lastEmail.sender).toBe("<from@estudafatec.com>");
+    expect(lastEmail.recipients[0]).toBe("<to@estudafatec.com>");
+    expect(lastEmail.subject).toBe("Last Email");
+    expect(lastEmail.text).toBe("Last email sent.\n");
   });
 });
