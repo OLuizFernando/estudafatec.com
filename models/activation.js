f8d67b2335360af7728c5a32a76cc3c6324a5cdd
OLuizFernando <luizfernandodematoscarvalho@gmail.com>
2025-10-20 19:35:30 -0300

    feat: send activation email after `user` registration

---
+++ models/activation.js
@@ -0,0 +1,82 @@
+import email from "infra/email";
+import postgres from "infra/postgres";
+import webserver from "infra/webserver";
+import { NotFoundError } from "infra/errors";
+
+const EXPIRATION_IN_MILLISECONDS = 60 * 15 * 1000; // 15 minutes
+
+async function generateToken(userId) {
+  const expiresAt = new Date(Date.now() + EXPIRATION_IN_MILLISECONDS);
+
+  const newToken = await runInsertQuery(userId, expiresAt);
+  return newToken;
+
+  async function runInsertQuery(userId, expiresAt) {
+    const results = await postgres.query({
+      text: `
+        INSERT INTO
+          user_activation_tokens (user_id, expires_at)
+        VALUES
+          ($1, $2)
+        RETURNING
+          *
+      ;`,
+      values: [userId, expiresAt],
+    });
+
+    return results.rows[0];
+  }
+}
+
+async function sendEmailToUser(user, activationToken) {
+  await email.send({
+    from: "EstudaFatec.com <contato@estudafatec.com>",
+    to: user.email,
+    subject: "Ative seu cadastro no EstudaFatec.com!",
+    text: `${user.name}, clique no link abaixo parra ativar seu cadastro no EstudaFatec.com:
+    
+${webserver.origin}/activation/${activationToken.id}
+
+Atenciosamente,
+Equipe EstudaFatec.com`,
+  });
+}
+
+async function findOneByUserId(userId) {
+  const activationTokenFound = await runSelectQuery(userId);
+
+  return activationTokenFound;
+
+  async function runSelectQuery(userId) {
+    const results = await postgres.query({
+      text: `
+        SELECT
+          *
+        FROM
+          user_activation_tokens
+        WHERE
+          user_id = $1
+        LIMIT
+          1
+        ;`,
+      values: [userId],
+    });
+
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O id informado não foi encontrado no sistema.",
+        action: "Verifique se o id está digitado corretamente.",
+      });
+    }
+
+    return results.rows[0];
+  }
+}
+
+const activation = {
+  generateToken,
+  sendEmailToUser,
+  findOneByUserId,
+};
+
+export default activation;
---
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -0,0 +1,56 @@
+import activation from "models/activation";
+import orchestrator from "tests/orchestrator";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+  await orchestrator.deleteAllEmails();
+});
+
+describe("Use case: Registration Flow (all successful)", () => {
+  let createUserResponseBody;
+
+  test("Create user account", async () => {
+    const createUserResponse = await fetch("http://localhost:3000/api/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: "Registration Flow",
+        email: "registration.flow@example.com",
+        password: "registrationflow",
+      }),
+    });
+
+    expect(createUserResponse.status).toBe(201);
+
+    createUserResponseBody = await createUserResponse.json();
+
+    expect(createUserResponseBody).toEqual({
+      id: createUserResponseBody.id,
+      name: "Registration Flow",
+      username: "registration-flow",
+      email: "registration.flow@example.com",
+      features: ["read:activation_token"],
+      hash: createUserResponseBody.hash,
+      created_at: createUserResponseBody.created_at,
+      updated_at: createUserResponseBody.updated_at,
+    });
+  });
+
+  test("Receive activation email", async () => {
+    const lastEmail = await orchestrator.getLastEmail();
+
+    const activationToken = await activation.findOneByUserId(
+      createUserResponseBody.id,
+    );
+
+    expect(lastEmail.sender).toBe("<contato@estudafatec.com>");
+    expect(lastEmail.recipients[0]).toBe("<registration.flow@example.com>");
+    expect(lastEmail.subject).toBe("Ative seu cadastro no EstudaFatec.com!");
+    expect(lastEmail.text).toContain("Registration Flow");
+    expect(lastEmail.text).toContain(activationToken.id);
+  });
+});
