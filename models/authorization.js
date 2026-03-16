bbc44cd4743226061bb156682a35c9acc0185d3a
Luiz Fernando <luizfernandodematoscarvalho@gmail.com>
2026-03-16 04:43:45 -0300

    feat: consider `resource` in `authorization` model

--- models/authorization.js
+++ models/authorization.js
@@ -1,5 +1,19 @@
-function can(userFeatures, requiredFeature) {
-  return userFeatures.includes(requiredFeature) ? true : false;
+function can(user, feature, resource) {
+  let authorized = false;
+
+  if (user.features.includes(feature)) {
+    authorized = true;
+  }
+
+  if (feature === "update:user" && resource) {
+    authorized = false;
+
+    if (user.id === resource.id) {
+      authorized = true;
+    }
+  }
+
+  return authorized;
 }
 
 const authorization = {
